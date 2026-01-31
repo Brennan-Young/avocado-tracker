577293e64d1218e59637817e77df8bb1d128452f
Brennan Young <m.brennanyoung@gmail.com>
2026-01-31 18:51:03 -0500

    Update to include price changes week to week

--- src/main/scala/Commodity.scala
+++ src/main/scala/Commodity.scala
@@ -1,5 +1,48 @@
 import upickle.{ReadWriter, macroRW}
 import ujson.Obj
+import java.math.MathContext
+import java.math.RoundingMode
+
+case class AvocadoPrice(
+  region: String,
+  variety: String,
+  organic: String,
+  averagePrice: Double,
+  previousPrice: Option[Double]
+) {
+
+  def priceChange = previousPrice.filter(_ != 0d).map: price =>
+    val c = BigDecimal(averagePrice)
+    val p = BigDecimal(price)
+    val percentChange = ((c - p) / p) * 100
+    percentChange.round(new MathContext(2, RoundingMode.HALF_UP))
+
+  def asSeq: Seq[String] = Seq(
+    region,
+    variety,
+    organic,
+    f"${averagePrice}%.2f",
+    previousPrice.map(p => f"${p}%.2f").getOrElse("-"),
+    priceChange match {
+      case Some(p) => (if (p >= 0) "+" else "") + p.toString + "%"
+      case None => "-"
+    }
+  )
+}
+
+case class ReportHeader(
+  results: List[Header]
+)
+
+object ReportHeader:
+  implicit val rw: ReadWriter[ReportHeader] = macroRW
+
+case class Header(
+  report_end_date: String
+)
+
+object Header:
+  implicit val rw: ReadWriter[Header] = macroRW
 
 case class ReportDetails(
   reportSection: String,
@@ -19,6 +62,7 @@ case class Commodity(
   size: String,
   variety: String,
   organic: String,
+  prior_WK_wtd_avg_price: Option[Double],
   wtd_avg_price: Double
 ) {
   def asSeq: Seq[String] = Seq(region, variety, organic, wtd_avg_price.toString)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,19 @@
 
 import scala.util.Try
 import ujson.{Obj, Arr}
+import java.time.format.DateTimeFormatter
+import java.time.LocalDate
 
 @main def hello(): Unit =
   val url = "https://marsapi.ams.usda.gov/services/v3.1/reports"
   val reportSlugId = 3324
   val queryParams: Map[String, String] = Map(
-    "lastReports" -> "1",
+    "lastReports" -> "2",
     "allSections" -> "true"
   )
 
   val usdaApiKey = Secrets.usdaApiKey
-
-  println("key size: " + usdaApiKey.size)
+  val dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy")
 
   val resp = requests.get(
     s"${url}/${reportSlugId}",
@@ -24,24 +25,88 @@ import ujson.{Obj, Arr}
 
   val respArrOpt = respJson.arrOpt
 
+  // print(respArrOpt.get.apply(0))
+
+  val reportEndDate = for {
+    arr <- respArrOpt
+    json <- arr.find: section =>
+      section.obj.get("reportSection")
+        .map(_.str) == Some("Report Header")
+    reportHeader <- Try(upickle.read[ReportHeader](json)).toOption
+    header <- reportHeader.results.headOption
+  } yield {
+    LocalDate.parse(header.report_end_date, dateFormat)
+  }
+
   val reportDetailsOpt = for {
+    endDate <- reportEndDate
     arr <- respArrOpt
     json <- arr.find: section =>
       section.obj.get("reportSection")
         .map(_.str) == Some("Report Details")
     reportDetails <- Try(upickle.read[ReportDetails](json)).toOption
-  } yield reportDetails
+  } yield (endDate, reportDetails)
 
-  val avocadosOpt = reportDetailsOpt.map: reportDetails =>
-    reportDetails.results.filter: commodity =>
+  val avocadosOpt = reportDetailsOpt.map: (endDate, reportDetails) =>
+    endDate -> reportDetails.results.filter: commodity =>
       commodity.commodity == "Avocados"
 
-  val avocadoPriceGroups = avocadosOpt.map: avocados =>
-    avocados.groupBy(_.region).map: (region, commodities) =>
-      region -> commodities.groupMap(_.size): commodity =>
-        commodity.wtd_avg_price
+  val avocadoGroups = avocadosOpt.map: (endDate, avocados) =>
+    avocados.filter: commodity =>
+      (commodity.region == "Northeast" || commodity.region == "National") && commodity.size == "each"
+    .groupBy: a =>
+      (a.region, a.size, a.variety, a.organic)
+    .flatMap: (group, avocados) =>
+      val sortedAvocados = avocados.sortBy(a => LocalDate.parse(a.report_end_date, dateFormat))(using Ordering[LocalDate].reverse)
 
-  val avocadoPrices = avocadosOpt.map: avocados =>
+      val firstAvocado = sortedAvocados.headOption
+      firstAvocado.flatMap: avocado =>
+        val date = LocalDate.parse(avocado.report_end_date, dateFormat)
+
+        date == endDate match
+          case true => Some(AvocadoPrice(
+            avocado.region,
+            avocado.variety,
+            avocado.organic,
+            avocado.wtd_avg_price,
+            if (sortedAvocados.size >= 2) Some(sortedAvocados(1).wtd_avg_price) else None
+          ))
+          case false => None
+
+  val deltaOrdering: Ordering[Option[BigDecimal]] =
+    Ordering.fromLessThan {
+      case (Some(a), Some(b)) => a < b        // normal numeric ordering
+      case (Some(_), None)    => true         // Some before None
+      case (None, Some(_))    => false
+      case (None, None)       => false
+    }
+
+  val sortedAvocadoPrices = avocadoGroups.map: avocados =>
+    avocados.toSeq.sortBy(a => a.priceChange)(using deltaOrdering)
+          
+  val payload = sortedAvocadoPrices.map: avocadoPrices =>
+    val avocadoStrings = avocadoPrices.map(avocado => avocado.asSeq)
+    
+    val table = SlackBlocks.table(
+      Seq("Region", "Variety", "Organic?", "Price", "Last Price", "Change"),
+      avocadoStrings
+    )
+
+    Obj(
+      "text" -> "Avocado Prices",
+      "blocks" -> Arr(
+        SlackBlocks.header("Avocado Prices"),
+        table
+      )
+    )
+   
+
+  // val avocadoPriceGroups = avocadosOpt.map: avocados =>
+  //   avocados.groupBy(_.region).map: (region, commodities) =>
+  //     region -> commodities.groupMap(_.size): commodity =>
+  //       commodity.wtd_avg_price
+
+  val avocadoPrices = avocadosOpt.map: (endDate, avocados) =>
     val avocadoStrings = avocados.filter: commodity =>
       (commodity.region == "Northeast" || commodity.region == "National") && commodity.size == "each"
     .map: commodity =>
@@ -67,10 +132,10 @@ import ujson.{Obj, Arr}
 
   val webhookUrl = Secrets.slackWebhook
 
-  avocadoPrices.foreach: payload =>
+  payload.foreach: p =>
     val postResp = requests.post(
       url = webhookUrl,
-      data = payload.render(),
+      data = p.render(),
       headers = Seq("Content-Type" -> "application/json")
     )
 
